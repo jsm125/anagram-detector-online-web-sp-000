bd4a43d43aca7f986bd8a629774d1c0fae6c70a3
Matt Im <matthew.seiim@gmail.com>
2020-05-10 20:39:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ def initialize(words)
   @words = words
 end
 
-def match(%w(words2))
+def match(words2)
   words2.each do |words|
     if words2.split.sort(//) == @words.split.sort(//)
       words2
