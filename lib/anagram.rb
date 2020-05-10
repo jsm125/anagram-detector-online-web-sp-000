d55cbaab9e82f9a86def93b06357ddae83967b97
Matt Im <matthew.seiim@gmail.com>
2020-05-10 19:54:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,15 @@
 # Your code goes here!
 class Anagram
 
+attr_accessor :words
+
+def initialize(words)
+  @words = words
+end
+
+def match(words2)
+  words2.each do |@words|
+    if words2.split.sort(//) == @words.split.sort(//)
+      true
 
 end
